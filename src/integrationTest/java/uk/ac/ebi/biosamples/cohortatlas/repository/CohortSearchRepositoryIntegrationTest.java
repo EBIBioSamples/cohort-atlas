e70fdc7ebf5af3f3ff129d7139436c74e959c15f
vijayaraja <vijayaraja@ebi.ac.uk>
2023-05-02 15:29:08 +0100

    added separate test case for cohort accessioning

--- src/integrationTest/java/uk/ac/ebi/biosamples/cohortatlas/repository/CohortSearchRepositoryIntegrationTest.java
+++ src/integrationTest/java/uk/ac/ebi/biosamples/cohortatlas/repository/CohortSearchRepositoryIntegrationTest.java
@@ -1,6 +1,6 @@
 package uk.ac.ebi.biosamples.cohortatlas.repository;
 
-import static org.junit.jupiter.api.Assertions.*;
+import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
@@ -11,9 +11,6 @@ import uk.ac.ebi.biosamples.cohortatlas.model.Cohort;
 import uk.ac.ebi.biosamples.cohortatlas.service.CohortService;
 
 import java.util.Collections;
-import java.util.List;
-
-import static org.junit.jupiter.api.Assertions.assertEquals;
 
 
 @SpringBootTest
@@ -40,13 +37,7 @@ class CohortSearchRepositoryIntegrationTest {
     cohortService.saveCohort(cohort);
 
     Page<Cohort> page = cohortSearchRepository.findPageWithFilters(PageRequest.of(0, 5), "CohortSearchRepositoryIntegrationTest", "cohortName", Collections.emptyList());
-    assertTrue(page.getSize() > 1);
-    assertEquals("SearchRepositoryIntegrationTest_freeTextSearch_1", page.getContent().get(0).getCohortName());
-
-    List<Cohort> cohorts = page.get().toList();
-    assertEquals("BSC000006",cohorts.get(0).getAccession());
-    assertEquals("SearchRepositoryIntegrationTest_freeTextSearch_1",cohorts.get(0).getCohortName());
-    assertEquals("BSC000007",cohorts.get(1).getAccession());
-    assertEquals("SearchRepositoryIntegrationTest_freeTextSearch_2",cohorts.get(1).getCohortName());
+    Assertions.assertTrue(page.getSize() > 1);
+    Assertions.assertEquals("SearchRepositoryIntegrationTest_freeTextSearch_1", page.getContent().get(0).getCohortName());
   }
 }
