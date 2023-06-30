ac224dcefc394ddb4a624e444cbb41a46f86b2fd
vijayaraja <vijayaraja@ebi.ac.uk>
2023-06-30 11:10:10 +0100

    filter api changes

--- src/integrationTest/java/uk/ac/ebi/biosamples/cohortatlas/repository/CohortSearchRepositoryIntegrationTest.java
+++ src/integrationTest/java/uk/ac/ebi/biosamples/cohortatlas/repository/CohortSearchRepositoryIntegrationTest.java
@@ -1,6 +1,6 @@
 package uk.ac.ebi.biosamples.cohortatlas.repository;
 
-import org.junit.jupiter.api.Assertions;
+import static org.junit.jupiter.api.Assertions.*;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
@@ -10,6 +10,7 @@ import org.springframework.test.context.ActiveProfiles;
 import uk.ac.ebi.biosamples.cohortatlas.model.Cohort;
 import uk.ac.ebi.biosamples.cohortatlas.service.CohortService;
 
+import java.util.Arrays;
 import java.util.Collections;
 
 
@@ -37,7 +38,59 @@ class CohortSearchRepositoryIntegrationTest {
     cohortService.saveCohort(cohort);
 
     Page<Cohort> page = cohortSearchRepository.findPageWithFilters(PageRequest.of(0, 5), "CohortSearchRepositoryIntegrationTest", "cohortName", Collections.emptyList());
-    Assertions.assertTrue(page.getSize() > 1);
-    Assertions.assertEquals("SearchRepositoryIntegrationTest_freeTextSearch_1", page.getContent().get(0).getCohortName());
+    assertTrue(page.getSize() > 1);
+    assertEquals("SearchRepositoryIntegrationTest_freeTextSearch_1", page.getContent().get(0).getCohortName());
   }
+
+  @Test
+  void filterCohorts() {
+    Cohort cohort = new Cohort();
+    cohort.setCohortName("first");
+    cohort.setDescription("first");
+    cohort.setAcronym("filter1");
+    cohortService.saveCohort(cohort);
+
+    cohort = new Cohort();
+    cohort.setCohortName("second");
+    cohort.setDescription("second");
+    cohort.setAcronym("filter2");
+    cohortService.saveCohort(cohort);
+
+    cohort = new Cohort();
+    cohort.setCohortName("third");
+    cohort.setDescription("third");
+    cohort.setAcronym("filter3");
+    cohortService.saveCohort(cohort);
+
+    //single filter
+    Page<Cohort> page = cohortSearchRepository.findPageWithFilters(PageRequest.of(0, 5), null,
+            "cohortName", Collections.singletonList("acronym:filter3"));
+    assertEquals(1, page.getContent().size());
+    assertEquals("third", page.getContent().get(0).getCohortName());
+
+    //single field multiple filter
+    page = cohortSearchRepository.findPageWithFilters(PageRequest.of(0, 5), null, "cohortName",
+            Collections.singletonList("acronym:filter3~filter2"));
+    assertEquals(2, page.getContent().size());
+
+    assertTrue((page.getContent().get(0).getCohortName().equals("second")
+            && page.getContent().get(1).getCohortName().equals("third"))
+    || (page.getContent().get(0).getCohortName().equals("third")
+            && page.getContent().get(0).getCohortName().equals("second")));
+
+
+    //multiple field multiple filter, second filter does not match with the first filter
+    page = cohortSearchRepository.findPageWithFilters(PageRequest.of(0, 5), null, "cohortName",
+            Arrays.asList("acronym:filter3~filter2","description:first"));
+    assertEquals(0, page.getContent().size());
+
+    //multiple field multiple filter. Only third cohort matches both acronym and description
+    page = cohortSearchRepository.findPageWithFilters(PageRequest.of(0, 5), null, "cohortName",
+            Arrays.asList("acronym:filter3~filter2","description:third"));
+    assertEquals(1, page.getContent().size());
+    assertEquals("third", page.getContent().get(0).getCohortName());
+
+  }
+
+
 }
--- src/main/java/uk/ac/ebi/biosamples/cohortatlas/repository/SearchRepository.java
+++ src/main/java/uk/ac/ebi/biosamples/cohortatlas/repository/SearchRepository.java
@@ -1,61 +1,66 @@
 package uk.ac.ebi.biosamples.cohortatlas.repository;
 
-import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.domain.Sort;
 import org.springframework.data.mongodb.core.MongoTemplate;
 import org.springframework.data.mongodb.core.query.Criteria;
 import org.springframework.data.mongodb.core.query.Query;
 import org.springframework.data.mongodb.core.query.TextCriteria;
-import org.springframework.data.support.PageableExecutionUtils;
 import org.springframework.stereotype.Repository;
 import org.springframework.util.CollectionUtils;
 import org.springframework.util.StringUtils;
-import uk.ac.ebi.biosamples.cohortatlas.model.Cohort;
 
+import java.util.Arrays;
 import java.util.List;
 
 @Repository
 public abstract class SearchRepository {
-  protected final MongoTemplate mongoTemplate;
+    protected final MongoTemplate mongoTemplate;
 
-  public SearchRepository(MongoTemplate mongoTemplate) {
-    this.mongoTemplate = mongoTemplate;
-  }
-
-  protected Query getSearchQuery(Pageable page, String text, String sort, List<String> filters) {
-    Query query = new Query().with(page);
-    populateQueryWithSort(query, sort);
-    populateQueryWithFreeTextSearch(query, text);
-    populateQueryWithFilters(query, filters);
-    return query;
-  }
-
-  private void populateQueryWithSort(Query query, String sort) {
-    if (StringUtils.hasText(sort)) {
-      query.with(Sort.by(sort).ascending());
-    }
-  }
-
-  private void populateQueryWithFreeTextSearch(Query query, String text) {
-    if (StringUtils.hasText(text)) {
-      query.addCriteria(TextCriteria.forDefaultLanguage().matchingAny(text));
-    }
-  }
-
-  private void populateQueryWithFilters(Query query, List<String> filters) {
-    if (CollectionUtils.isEmpty(filters)) {
-      return;
+    public SearchRepository(MongoTemplate mongoTemplate) {
+        this.mongoTemplate = mongoTemplate;
     }
 
-    for (String filter : filters) {
-      String[] filterParts = filter.split(":");
-      if (filterParts.length > 1) {
-        String field = filterParts[0];
-        String value = filterParts[1];
-        query.addCriteria(Criteria.where(field).is(value));
-      }
+    protected Query getSearchQuery(Pageable page, String text, String sort, List<String> filters) {
+        Query query = new Query().with(page);
+        populateQueryWithSort(query, sort);
+        populateQueryWithFreeTextSearch(query, text);
+        populateQueryWithFilters(query, filters);
+        return query;
     }
-  }
+
+    private void populateQueryWithSort(Query query, String sort) {
+        if (StringUtils.hasText(sort)) {
+            query.with(Sort.by(sort).ascending());
+        }
+    }
+
+    private void populateQueryWithFreeTextSearch(Query query, String text) {
+        if (StringUtils.hasText(text)) {
+            query.addCriteria(TextCriteria.forDefaultLanguage().matchingAny(text));
+        }
+    }
+
+    private void populateQueryWithFilters(Query query, List<String> filtersList) {
+        if (CollectionUtils.isEmpty(filtersList)) {
+            return;
+        }
+
+        for (String filtersStr : filtersList) {
+            String[] filterArray = filtersStr.split(":");
+            if (filterArray.length > 1) {
+                String filterField = filterArray[0];
+                String[] filterByValues = filterArray[1].split("~");
+
+                if (filterByValues.length > 1) {
+                        query.addCriteria(Criteria.where(filterField).in(Arrays.asList(filterByValues)));
+                } else {
+                    query.addCriteria(Criteria.where(filterField).is(filterByValues[0]));
+                }
+            }
+        }
+
+    }
+
 
 }
